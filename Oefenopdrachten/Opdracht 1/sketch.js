ccd1969fb42824f5c7177f445ad125ca529f13a2
Jordyluit <jordy@spinsteen.nl>
2026-09-08 13:16:41 +0200

    Mondriaan oefenopdracht

--- Oefenopdrachten/Opdracht 1/sketch.js
+++ Oefenopdrachten/Opdracht 1/sketch.js
@@ -1,7 +1,44 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(606, 406);
 }
 
 function draw() {
-  background(220);
+  background(255);
+  strokeWeight (4)
+  fill("nofill");
+  rect (4,4,602,402);
+  fill("nofill");
+  rect (4,4,350,50);
+  fill ("blue");
+  rect (4,4,160,50);
+  fill("yellow");
+  rect (280,4,100,50);
+  fill("red")
+   rect (330,4,80,50);
+   fill("nofill")
+    rect (410,4,80,50);
+    fill("black")
+     rect (490,4,32,200);
+     fill("red")
+      rect (522,4,80,200);
+       rect (4,54,80,50);
+       fill("nofill")
+        rect (84,54,80,50);
+        fill("blue")
+         rect (164,54,167,150);
+         fill("nofill")
+         rect (359,54,131,150);
+         rect (4,104,160,100)
+         fill("yellow")
+         rect (4,204,160,100)
+         fill("nofill")
+         rect (4,304,240,100)
+         fill("red")
+           rect (245,204,160,100)
+           fill("black")
+             rect (324,204,165,100)
+             fill("blue")
+             rect (489,204,116,100)
+             rect (244,304,247,100)
+         
 }
--- Week 1/sketch.js
+++ Week 1/sketch.js
@@ -1,17 +1,3 @@
 function setup() {
-  createCanvas(400, 400);
-}
-
-function draw() {
-  background("lightblue");
-  fill ("grey");
-  rect (150,300,50,100);
-  fill ("grey");
-  rect (125,110,100,200);
-  fill ("red")
-  circle (174,150,50)
-  fill ("orange")
-circle (174,210,50);
-  fill ("green")
-  circle (174,270,50);  
+  createCanvas(600, 400);
 }
