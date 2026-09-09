9716b785ba38ca0ce19f0e2cc57e2192533d6956
Jordyluit <jordy@spinsteen.nl>
2026-09-09 14:02:28 +0200

    albumcover

---
+++ Oefenopdrachten/extra opdrachten/sketch.js
@@ -0,0 +1,37 @@
+function setup() {
+  createCanvas(1100, 1100);
+}
+
+function draw(){
+ background (220);
+ fill("black");
+ square(0,0,700);
+ fill("transparency");
+ circle(350,350,550);
+ fill("black");
+ circle(350,350,500);
+ fill("white");
+ circle(350,350,450);
+ fill("black")
+  circle(350,350,400);
+  fill("white");
+   circle(350,350,350);
+   fill("black");
+    circle(350,350,300);
+    fill("white")
+     circle(350,350,250);
+     fill("black");
+      circle(350,350,200);
+      fill("white");
+       circle(350,350,150);
+       fill("black");
+        circle(350,350,100);
+        fill("white")
+         circle(350,350,50);
+fill("red");
+ textSize(800);
+ text("Y",85,650);
+ fill("red");
+ textSize(70);
+ text("Teach me to fight",85,55);
+}
