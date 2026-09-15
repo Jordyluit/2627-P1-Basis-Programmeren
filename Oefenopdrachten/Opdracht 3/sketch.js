4fff9a5cffae45d5ce2e5078a83b5da7cc852287
Jordyluit <jordy@spinsteen.nl>
2026-09-15 11:22:01 +0200

    Update sketch.js

--- Oefenopdrachten/Opdracht 3/sketch.js
+++ Oefenopdrachten/Opdracht 3/sketch.js
@@ -4,5 +4,21 @@ function setup() {
 
 function draw() {
   background(220);
-  let score = 90
+  let score = random(100)
+  noLoop (score) 
+  text (score,20,20)
+  if (score > 90) {
+  console.log("Uitstekend!");
+}
+
+if (score >=70 && score <=89 ) {
+  console.log("Goed Gedaan!");
+}
+
+if (score >= 60 && score <=69) {
+  console.log("voldoende");
+}
+if (score <=59) {
+  console.log("onvoldoende");
+}
 }
