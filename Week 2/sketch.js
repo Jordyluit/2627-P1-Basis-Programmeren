509f743f79fa4066ded6a6244fa55c7aa5e8667c
Jordyluit <jordy@spinsteen.nl>
2026-09-18 09:46:25 +0200

    Update sketch.js

--- Week 2/sketch.js
+++ Week 2/sketch.js
@@ -17,6 +17,15 @@ let car1Speed = 3
 let car2Speed = 3;
 let car3Speed = -6;
 
+let wolk1X = 0
+let wolk1Snelheid = -2
+
+let wolk2X = 0
+let wolk2Snelheid = -4
+
+let wolk3X = 0
+let wolk3Snelheid = -1
+
 
 let groen = false
 function setup() {
@@ -78,59 +87,7 @@ fill("#D6D6D6")
   //gras langs de snelweg
   fill("green")
   rect(0, 400, 800, 200)
-
-  //snelweg
-  fill("grey")
-  rect(0, 450, 800, 100)
-  fill("white")
-  rect(0, 490, 50, 25)
-  rect(100, 490, 50, 25)
-  rect(200, 490, 50, 25)
-  rect(300, 490, 50, 25)
-  rect(400, 490, 50, 25)
-  rect(500, 490, 50, 25)
-  rect(600, 490, 50, 25)
-  rect(700, 490, 50, 25)
-  rect(800, 490, 50, 25)
-
- //auto 3
-  fill("yellow")
-  rect(50+car3X,370,200,100)
-  fill("lightblue")
-  rect(50+ car3X,370,25,50)
-  fill("black")
-  circle(70+ car3X,470,50)
-   circle(230+ car3X,470,50)
-  if (car3X <= -300)
-    car3X= 775
-  //auto1
-  fill("orange")
-  rect(20 + car1X, 490, 100, 50)
-  fill("lightblue")
-  rect(75 + car1X, 500, 45, 20)
-  fill("black")
-  circle(40 + car1X, 540, 25)
-  circle(100 + car1X, 540, 25)
-  if (car1X >= 775)
-    car1X = -200
-
-  //auto 2
-
-  strokeWeight (0)
-  fill("pink")
-  rect(40+ car2X, 490,150,50)
-  rect(40 + car2X, 440,75,50)
-  fill("lightblue")
-  rect(115 + car2X, 440,75,50)
-  fill("black")
-  circle(60 + car2X,540,25) 
-  circle(170 + car2X,540,25)
-  if (car2X >= 775)
-car2X =-200
-
-
- 
- 
+   
 //TrafficLight
   // Pole
   fill(60);
@@ -165,6 +122,85 @@ car2X =-200
   }
 
   circle(740, 315, 40);
+     //snelweg
+  fill("grey")
+  rect(0, 450, 800, 100)
+  fill("white")
+  rect(0, 490, 50, 25)
+  rect(100, 490, 50, 25)
+  rect(200, 490, 50, 25)
+  rect(300, 490, 50, 25)
+  rect(400, 490, 50, 25)
+  rect(500, 490, 50, 25)
+  rect(600, 490, 50, 25)
+  rect(700, 490, 50, 25)
+  rect(800, 490, 50, 25)
+
+ //auto 3
+  fill("yellow")
+  rect(50+car3X,370,200,100,25)
+  fill("lightblue")
+  rect(50+ car3X,370,25,50)
+  fill("black")
+  circle(70+ car3X,470,50)
+   circle(230+ car3X,470,50,)
+  if (car3X <= -300)
+    car3X= 775
+  //auto1
+  fill("orange")
+  rect(20 + car1X, 490, 100, 50,90)
+  fill("lightblue")
+  rect(75 + car1X, 500, 45, 20,90)
+  fill("black")
+  circle(40 + car1X, 540, 25)
+  circle(100 + car1X, 540, 25)
+  if (car1X >= 775)
+    car1X = -200
+
+  //auto 2
+
+  strokeWeight (0)
+  fill("pink")
+  rect(40+ car2X, 490,150,50)
+  rect(40 + car2X, 440,75,50)
+  fill("lightblue")
+  rect(115 + car2X, 440,75,50)
+  fill("black")
+  circle(60 + car2X,540,25) 
+  circle(170 + car2X,540,25)
+  if (car2X >= 775)
+car2X =-200
+
+  //wolken
+
+  fill(239,243,229)
+  //wolk 1
+
+  circle(50+ wolk1X,50,50)
+  circle(75+ wolk1X,50,75)
+  circle(100+ wolk1X,50,50)
+
+  wolk1X = wolk1X + wolk1Snelheid
+  if (wolk1X <= -150)
+    wolk1X = 790
+  
+  //wolk 2
+  circle(150+ wolk2X,100,50)
+  circle(175+ wolk2X,100,75)
+  circle(200+ wolk2X,100,50)
+
+  wolk2X = wolk2X + wolk2Snelheid
+  if (wolk2X <= -250)
+    wolk2X = 790
+
+  //wolk 3
+  circle(550+ wolk3X,50,50)
+  circle(575+ wolk3X,50,75)
+  circle(600+ wolk3X,50,50)
+
+   wolk3X = wolk3X + wolk3Snelheid
+  if (wolk3X <= -7000)
+    wolk3X = 750
 }
 function moveCars() {
 
@@ -181,17 +217,8 @@ function moveCars() {
     car3X += car3Speed * 0.5;
   }
 
-  // if (car1X > width + 150) {
-  //   car1X = -150;
-  // }
 
-  // if (car2X > width + 150) {
-  //   car2X = -150;
-  // }
 
-  // if (car3X > width + 150) {
-  //   car3X = -150;
-  // }
 }
 function keyPressed() {
   if (keyCode === ENTER) {
@@ -208,4 +235,8 @@ function keyPressed() {
     }
   }
 
+
+ 
+
+ 
 }
